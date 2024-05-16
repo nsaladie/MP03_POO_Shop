ce8cdda51f720ad4845e160f027906bdbf5438e0
nsala <nsala@Noe>
2024-05-16 09:25:02 +0200

    Edit the name of the attribute

--- src/exception/LimitLoginException.java
+++ src/exception/LimitLoginException.java
@@ -2,12 +2,12 @@ package exception;
 
 public class LimitLoginException extends Exception {
 
-	private static String message = "Maximum number of login attempts reached. The application will close";
+	private static String messageLoginAttemp = "Maximum number of login attempts reached. The application will close";
 	private static final long serialVersionUID = 1L;
 
 	// Call the constructor
 	public LimitLoginException() {
-		super(message);
+		super(messageLoginAttemp);
 	}
 
 }
