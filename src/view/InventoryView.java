002d495367c62bae8e5f9414b3207bf4d7ab4563
nsala <nsala@Noe>
2024-05-16 20:35:27 +0200

    Update InventoryView class
    - Renamed the dialog title to "Inventory Shop" for clarity and
    consistency.
    - Modified the table model to disable user editing of the table content.
    - Added a method capitalizeFirstLetter() to capitalize the first letter
    of product names for better readability.
    - Reorganized and improved comments for better code readability and
    maintainability.

--- src/view/InventoryView.java
+++ src/view/InventoryView.java
@@ -37,6 +37,7 @@ public class InventoryView extends JDialog implements ActionListener {
 	 */
 	public InventoryView(Shop shop) {
 		this.shop = shop;
+		setTitle("Inventory Shop");
 
 		setBounds(100, 100, 540, 460);
 		getContentPane().setLayout(new BorderLayout());
@@ -51,12 +52,18 @@ public class InventoryView extends JDialog implements ActionListener {
 		panel.setLayout(null);
 		contentPanel.add(panel);
 
+		// Define column names for the table
 		String[] columnNames = { "Id Product", "Product Name", "Product Price", "WholeSale Price", "Stock" };
+		// Create a new DefaultTableModel with specified column names and 0 rows
 		model = new DefaultTableModel(columnNames, 0);
+		// Create a JTable with a DefaultTableModel containing an empty data array and
+		// the same column names
 		table = new JTable(new DefaultTableModel(new Object[][] {},
 				new String[] { "Id Product", "Product Name", "Product Price", "WholeSale Price", "Stock" }));
-		table.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		table.setEnabled(false);
 
+		table.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		// Set auto resize mode for the table columns
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 
 		// Add Scroll to the Table
@@ -64,13 +71,14 @@ public class InventoryView extends JDialog implements ActionListener {
 		scrollPane.setBounds(0, 50, 506, 280);
 		panel.add(scrollPane);
 
+		// Call the method loadInventory()
 		loadInventory();
 
-		JLabel lblNewLabel = new JLabel("Inventory - Shop");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
-		lblNewLabel.setBounds(0, 10, 176, 30);
-		panel.add(lblNewLabel);
+		JLabel lblTitle = new JLabel("Inventory - Shop");
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setFont(new Font("SansSerif", Font.BOLD, 20));
+		lblTitle.setBounds(0, 10, 176, 30);
+		panel.add(lblTitle);
 
 		backButton = new JButton("Back");
 		backButton.setFont(new Font("SansSerif", Font.PLAIN, 20));
@@ -81,13 +89,31 @@ public class InventoryView extends JDialog implements ActionListener {
 	}
 
 	private void loadInventory() {
+		// Get the information of shop.getInventory() and saved in an ArrayList
 		inventory = shop.getInventory();
-
+		// for each that runs through all the ArrayList of inventory
 		for (Product product : inventory) {
-			model.addRow(new Object[] { product.getStock(), product.getPublicPrice().amountSale(),
-					product.getWholesalerPrice().amountSale(), product.getName(), product.getId() });
+			// Add a new row with this information
+			model.addRow(new Object[] { 
+					product.getId(), // Product ID
+					capitalizeFirstLetter(product.getName()), // Product name
+					product.getPublicPrice().amountSale(), // Product public price
+					product.getWholesalerPrice().amountSale(), // Product wholesale price
+					product.getStock() // Product stock
+			});
+			// Update the table model after adding each row
 			table.setModel(model);
 		}
+	}
+
+	/*
+	 *  Put the first character in upper 
+	 */
+	private String capitalizeFirstLetter(String str) {
+		if (str == null || str.isEmpty()) {
+			return str;
+		}
+		return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
 	}
 
 	@Override
