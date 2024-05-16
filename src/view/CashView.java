108105ea8b79a42907f9b847b1661a30b5befc71
nsala <nsala@Noe>
2024-05-16 09:12:50 +0200

    Create CashView to display shop's total cash
    - Implements a dialog to show the total cash of the shop
    - Uses a JTextField to display the cash amount retrieved from the Shop
    class

---
+++ src/view/CashView.java
@@ -0,0 +1,70 @@
+package view;
+
+import main.Shop;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class CashView extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private JPanel panel;
+	private JTextField cash;
+	private Shop shop;
+	private int option;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public CashView(Shop shop, int option) {
+		this.shop = shop;
+		this.option = option;
+
+		setBounds(100, 100, 360, 350);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(Color.DARK_GRAY);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			panel = new JPanel();
+			panel.setBounds(39, 21, 260, 260);
+			contentPanel.add(panel);
+			panel.setLayout(null);
+
+			JLabel lblImage = new JLabel("");
+			lblImage.setHorizontalAlignment(SwingConstants.CENTER);
+			lblImage.setIcon(new ImageIcon(CashView.class.getResource("/resorce/caja-registradora.png")));
+			lblImage.setBounds(0, 62, 260, 128);
+			panel.add(lblImage);
+
+			cash = new JTextField();
+			cash.setEditable(false);
+			cash.setHorizontalAlignment(SwingConstants.CENTER);
+			cash.setText(shop.getCash());
+			cash.setFont(new Font("SansSerif", Font.PLAIN, 20));
+			cash.setBounds(64, 211, 133, 26);
+			panel.add(cash);
+			cash.setColumns(10);
+
+			JLabel lblTitle = new JLabel("Total Cash");
+			lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+			lblTitle.setFont(new Font("SansSerif", Font.BOLD, 25));
+			lblTitle.setBounds(0, 10, 260, 31);
+			panel.add(lblTitle);
+		}
+	}
+}
