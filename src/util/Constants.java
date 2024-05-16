5cb39d8ef73f39897644083437b508594a3afd22
nsala <nsala@Noe>
2024-05-16 09:28:07 +0200

    - Correct duplicate value for UPDATE_STOCK and SHOW_INVENTORY

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 	public static final int SHOW_CASH = 1;
 	public static final int ADD_PRODUCT = 2;
 	public static final int UPDATE_STOCK = 3;
-	public static final int SHOW_INVENTORY = 3;	
+	public static final int SHOW_INVENTORY = 5;	
 	public static final int DELETE_PRODUCT = 9;
 }
