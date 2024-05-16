14730f9539b55d56f457bdfe8abe987a5d5855ed
nsala <nsala@Noe>
2024-05-16 09:42:26 +0200

    Update LoginView UI and functionality
    - Updated font styles and sizes for labels, text fields, and buttons to
    use a consistent SansSerif font
    - Added asterisks (*) as indicators for required fields (User ID and
    Password)
    - Added input validation to ensure User ID and Password fields are not
    empty
    - Renamed 'LoginView' title to 'Login'
    - Updated 'ShopView' title to 'Menu - Shop'

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -18,6 +18,8 @@ public class LoginView extends JFrame implements ActionListener {
 	private JButton btnReset;
 	private int loginAttempts = 0;
 	private static final int MAX_LOGIN_ATTEMPTS = 3;
+	private JLabel lblSymbol;
+	private JLabel lblSymbol2;
 
 	/**
 	 * Launch the application.
@@ -28,6 +30,7 @@ public class LoginView extends JFrame implements ActionListener {
 				try {
 					LoginView frame = new LoginView();
 					frame.setVisible(true);
+					frame.setTitle("Login");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -59,44 +62,58 @@ public class LoginView extends JFrame implements ActionListener {
 
 		JLabel Login = new JLabel("Login");
 		Login.setHorizontalAlignment(SwingConstants.LEFT);
-		Login.setFont(new Font("Tahoma", Font.BOLD, 30));
+		Login.setFont(new Font("SansSerif", Font.BOLD, 30));
 		Login.setBounds(24, 39, 111, 37);
 		panel.add(Login);
 
 		JLabel labelUser = new JLabel("User ID");
-		labelUser.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		labelUser.setBounds(73, 124, 96, 31);
+		labelUser.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		labelUser.setBounds(73, 124, 85, 31);
 		panel.add(labelUser);
 
 		JLabel labelPassword = new JLabel("Password");
-		labelPassword.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		labelPassword.setBounds(73, 208, 96, 30);
+		labelPassword.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		labelPassword.setBounds(73, 208, 91, 30);
 		panel.add(labelPassword);
 
 		// Create input userID
 		userID = new JTextField();
-		userID.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		userID.setFont(new Font("SansSerif", Font.PLAIN, 18));
 		userID.setBounds(73, 167, 213, 31);
 		panel.add(userID);
 		userID.setColumns(10);
 
 		// Create input password
 		passwordField = new JPasswordField();
-		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		passwordField.setFont(new Font("SansSerif", Font.PLAIN, 18));
 		passwordField.setBounds(73, 248, 213, 30);
 		panel.add(passwordField);
 
 		btnLogin = new JButton("Login");
-		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnLogin.setBounds(73, 329, 85, 31);
+		btnLogin.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		btnLogin.setBounds(73, 329, 91, 31);
 		btnLogin.addActionListener(this);
 		panel.add(btnLogin);
 
 		btnReset = new JButton("Reset");
-		btnReset.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnReset.setBounds(201, 329, 85, 31);
+		btnReset.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		btnReset.setBounds(201, 329, 91, 31);
 		btnReset.addActionListener(this);
 		panel.add(btnReset);
+		
+		lblSymbol = new JLabel("*");
+		lblSymbol.setForeground(Color.RED);
+		lblSymbol.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblSymbol.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSymbol.setBounds(139, 124, 21, 26);
+		panel.add(lblSymbol);
+		
+		lblSymbol2 = new JLabel("*");
+		lblSymbol2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSymbol2.setForeground(Color.RED);
+		lblSymbol2.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblSymbol2.setBounds(160, 208, 21, 26);
+		panel.add(lblSymbol2);
 
 	}
 
@@ -109,6 +126,11 @@ public class LoginView extends JFrame implements ActionListener {
 		String identificationUser = userID.getText().trim();
 		String password = String.valueOf(passwordField.getPassword()).trim();
 
+		if(identificationUser.isEmpty() || password.isEmpty()) {
+			JOptionPane.showMessageDialog(this, "The userID or password can't be null", "Invalid Type: NULL", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		
 		// Check if the user entered only numeric characters in the UserID field
 		if (isNumeric(identificationUser)) {
 			int user = Integer.parseInt(identificationUser);
@@ -127,6 +149,8 @@ public class LoginView extends JFrame implements ActionListener {
 					ShopView shopView = new ShopView();
 					// Put visible the view of ShopView
 					shopView.setVisible(true);
+					// Put a title to the view
+					shopView.setTitle("Menu - Shop");
 					// Restore keyboard focus for the shop window
 					shopView.requestFocus();
 					// Close the view of LoginView
