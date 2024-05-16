a3e1cdd1a1ee47a7a47075155634ebdd71b12b6f
nsala <nsala@Noe>
2024-05-16 20:10:40 +0200

    Add new constant for maximum login attempts
    Create a new constant MAX_LOGIN_ATTEMPTS in the Constants class to
    define the maximum number of login attempts allowed.

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
 	public static final int SHOW_CASH = 1;
 	public static final int ADD_PRODUCT = 2;
 	public static final int UPDATE_STOCK = 3;
-	public static final int SHOW_INVENTORY = 5;	
+	public static final int SHOW_INVENTORY = 5;
 	public static final int DELETE_PRODUCT = 9;
+
+	public static final int MAX_LOGIN_ATTEMPTS = 3;
 }
