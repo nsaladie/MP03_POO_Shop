119c588ef7a8674c2a808fd270d6d84be539d4fa
nsala <nsala@Noe>
2024-05-29 10:52:43 +0200

    Refactoring in the Employee class:
    - Removed constant variables.
    - Added a new attribute Dao.
    - Commented out the previous version of the login method using constant
    variables.
    - Modified the login method to use dao.connect to establish a connection
    to the database, dao.getEmployee to check if the login attempt in
    loginView matches the data stored in the database, and dao.disconnect to
    disconnect from the database.

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,35 +1,64 @@
 package model;
 
+import java.sql.SQLException;
+
+import dao.Dao;
+import dao.DaoImplJDBC;
 import main.Logable;
 
 public class Employee extends Person implements Logable {
 	// Constants
-	final private int EMPLOYEEDID = 123;
-	final private String PASSWORD = "test";
+	// final private int EMPLOYEEDID = 123;
+	// final private String PASSWORD = "test";
 
-	private int emplyeeId = EMPLOYEEDID;
+	private int employeeId;
+	private String password;
+
+	private Dao dao = new DaoImplJDBC();
+
+	public Employee(int id, String name, String pw) {
+		super(name);
+		// TODO Auto-generated constructor stub
+		this.employeeId = id;
+		this.password = pw;
+	}
+
+	public Employee() {
+
+	}
 
 	public int getEmplyeeId() {
-		return emplyeeId;
+		return employeeId;
 	}
 
 	public void setEmplyeeId(int emplyeeId) {
-		this.emplyeeId = emplyeeId;
+		this.employeeId = emplyeeId;
 	}
 
-	public int getEMPLOYEEDID() {
-		return EMPLOYEEDID;
-	}
+	@Override
+	public boolean login(int user, String password) {
+		// TODO Auto-generated method stub
+		try {
+			dao.connect();
+			Employee employee = dao.getEmployee(user, password);
 
-	public String getPASSWORD() {
-		return PASSWORD;
+			if (employee != null) {
+				dao.disconnect();
+				return true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+		return false;
 	}
 
 	// Check if the user and password send by user are equals to the constants
-	public boolean login(int user, String password) {
-		if (user == EMPLOYEEDID && password.equals(PASSWORD)) {
-			return true;
-		}
-		return false;
-	}
+	// public boolean login(int user, String password) {
+	// if (user == EMPLOYEEDID && password.equals(PASSWORD)) {
+	// return true;
+	// }
+	// return false;
+	// }
 }
